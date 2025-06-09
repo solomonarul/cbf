e68e147d041f567565ce7a99cc83560bc4f473b7
Solomonarul <solomonarul@tuta.io>
2025-06-09 23:36:01 +0300

    feat(bench): Add another test and paralelize.

--- bench/main.c
+++ bench/main.c
@@ -5,9 +5,10 @@
 #endif
 #include "interpreter.c"
 
-size_t TEST_COUNT = 1;
+size_t TEST_COUNT = 2;
 
 char* tests[] = {
+    "alphabet.b",
     "mandlebrot.b"
 };
 
@@ -15,32 +16,62 @@ char mandlebrot[] = {
     #embed "mandlebrot.b"
 };
 
-char* test_sources[] = {
-    mandlebrot
+char alphabet[] = {
+    #embed "alphabet.b"
 };
+
+char* test_sources[] = {
+    alphabet,
+    mandlebrot,
+};
+
+typedef struct {
+    char* test_name;
+    char* test_source;
+    int optimization_level;
+} ThreadArgs;
+
+int run_bench(void* ptr)
+{
+    ThreadArgs* const args = (ThreadArgs*)ptr;
+
+#ifdef JIT_LIGHTNING
+    bf_lightning_run_bench(args->test_name, args->test_source, args->optimization_level);
+#endif
+    bf_interpreter_run_bench(args->test_name, args->test_source, args->optimization_level);
+
+    return 0;
+}
 
 int main(int argc, char* argv[])
 {
     UNUSED(argc);
     auxum_set_app_path(argv[0]);
 
+    SDL_Thread* threads[TEST_COUNT];
+    ThreadArgs args[TEST_COUNT];
+
     printf("[BNCH]: Optimization level: INSTRUCTION_FOLDING\n");
     for(size_t index = 0; index < TEST_COUNT; index++)
     {
-#ifdef JIT_LIGHTNING
-        bf_lightning_run_bench(tests[index], test_sources[index], BF_OPTIMIZATIONS_INSTRUCTION_FOLDING);  
-#endif 
-        bf_interpreter_run_bench(tests[index], test_sources[index], BF_OPTIMIZATIONS_INSTRUCTION_FOLDING);
+        args[index].test_name = tests[index];
+        args[index].test_source = test_sources[index];
+        args[index].optimization_level = BF_OPTIMIZATIONS_INSTRUCTION_FOLDING;
+        threads[index] = SDL_CreateThread(run_bench, "BenchThread", &args[index]);
     }
+    for (size_t index = 0; index < TEST_COUNT; index++)
+        SDL_WaitThread(threads[index], NULL);
 
     printf("[BNCH]: Optimization level: NONE\n");
     for(size_t index = 0; index < TEST_COUNT; index++)
     {
-#ifdef JIT_LIGHTNING
-        bf_lightning_run_bench(tests[index], test_sources[index], BF_OPTIMIZATIONS_NONE);  
-#endif     
-        bf_interpreter_run_bench(tests[index], test_sources[index], BF_OPTIMIZATIONS_NONE);
+        args[index].test_name = tests[index];
+        args[index].test_source = test_sources[index];
+        args[index].optimization_level = BF_OPTIMIZATIONS_NONE;
+        threads[index] = SDL_CreateThread(run_bench, "BenchThread", &args[index]);
     }
+    for (size_t index = 0; index < TEST_COUNT; index++)
+        SDL_WaitThread(threads[index], NULL);
 
     return 0;
 }
